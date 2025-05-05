68e7f4458080f771043f1115fdc2a068394fc020
uwinezagloria <gloria7wineza.com>
2025-05-05 23:02:22 +0200

    updating

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,16 +10,15 @@ heading.style.color="white"
      heading.innerText=currentValue;
     }
     function operator(oper){
-        // Check if the last character is an operator or if the operator is already an operator
+      
     if (currentValue.slice(-1) === '+' || currentValue.slice(-1) === '-' ||
     currentValue.slice(-1) === '*' || currentValue.slice(-1) === '/') {
     
-    // If the last character is an operator, don't append another one
+    
     alert("You cannot enter two operators consecutively!");
     return;
 }
 
-// Check if the operator is '++' or '--' (invalid)
 if (oper === '++' || oper === '--') {
     alert("Invalid operator!");
     return;
